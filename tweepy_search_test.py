23f7cc9f2eb3cf8e221aa4a5a0b972bed032048c
JingShing <JingShingLai@gmail.com>
2022-11-30 21:53:30 +0800

    Add files via upload

---
+++ tweepy_search_test.py
@@ -0,0 +1,18 @@
+import tweepy
+from private_key import secret_key, api_key, api_secret, access_key, access_secret
+ 
+# fill the develop key from twitter develop
+consumer_key = api_key
+consumer_secret = api_secret
+access_token = access_key
+access_token_secret = access_secret
+ 
+# submit your key and secret key
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+ 
+# get contain text post
+api = tweepy.API(auth)
+
+for tweets in api.search_tweets(q="PixelArtFilterWeb", lang="en"):
+    print(tweets.text.split(' ')[-1])
