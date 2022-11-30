26bd4d2f4655bca729a3d7002321762f543fc842
JingShing <JingShingLai@gmail.com>
2022-11-30 21:55:30 +0800

    Create private_key.py

---
+++ scripts/private_key.py
@@ -0,0 +1,7 @@
+secret_key = 'A_KEY_FOR_FLASK'
+# api key is consumer key
+api_key='api_key'
+api_secret='api_secret_key'
+# access keys
+access_key = 'access_key'
+access_secret = 'access_secret_key'
